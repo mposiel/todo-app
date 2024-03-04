acccd52dc4296d2136f2e18330c29f91e0b736cc
mposiel <114227935+mposiel@users.noreply.github.com>
2024-03-04 20:58:03 +0100

    Inirial commit

---
+++ src/main/java/com/springboot/todoapp/hello/SayHelloController.java
@@ -0,0 +1,11 @@
+package com.springboot.todoapp.hello;
+
+public class SayHelloController {
+
+    //"say-hello" => "Hello! What are you learning today?"
+
+    public String sayhello() {
+        return "Hello! What are you learning today?";
+    }
+
+}
